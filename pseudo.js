b3fd842992ade45decdbda40a03a0716bb39cbac
Jon Owens <jonowens84@gmail.com>
2020-10-02 10:39:35 +0100

    html page set up and pseudo

---
+++ pseudo.js
@@ -0,0 +1,22 @@
+/*build an interactive website, with randomized dad jokes
+when viewer sees the dad joke upvote or downvote. edit an image
+or sound to react to the joke. If terrible have groan and sad face
+if funny have laughing and happy face.
+
+1) link dad jokes api using. 
+
+fetch("https://icanhazdadjoke.com/", {
+  headers: { accept: "application/json" },
+});
+
+
+2) create two buttons to vote the joke up or down
+
+
+3) setTimeout to have button play reaction then chnge the joke.
+
+
+3b)alt..have a button to end the function and set new joke
+
+
+4) reset after reaction then replace the joke
