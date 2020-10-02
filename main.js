ab6bf1a0b514356aad667732a5809d75063ad6eb
Emma randle <emmarandle@Emmas-iMac.local>
2020-10-02 12:01:34 +0100

    harold change

--- main.js
+++ main.js
@@ -10,16 +10,12 @@ async function getJoke(){
 }
 let h2 = document.querySelector("h2")
 getJoke()
-// h2.innerText = getJoke();
 
-let upVote = document.querySelector("#upVote");
-let downVote = document.querySelector("#downVote");
-upVote.addEventListener("click");
-downVote.addEventListener("click");
 let upPic = document.querySelector("#upPic");
 let downPic = document.querySelector("#downPic");
-upPic.addEventListener("click")
-downPic.addEventListener("click")
+
+upPic.addEventListener("click", upReaction);
+downPic.addEventListener("click", downReaction);
 
 /*write a function to add a reaction to the joke when the viewer clicks upvote
 write a function to add a reaction to the joke when the viewer clicks downvote
@@ -28,7 +24,12 @@ when clicks display corresponding image and sound effect..
 if working and is too repetetive add more reactions to add variety.. 
 
 */
-function upReaction (){
-  upPic
+function upReaction(){
+  upPic.src = "harold_up1.jpg";
+  //play audio
+}
 
+function downReaction(){
+  downPic.src = "harold_down.jpeg";
+  //play audio
 }
