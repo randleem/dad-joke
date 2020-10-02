e3fab496914762f9ea462ddf11ca4a107e969fe1
Jon Owens <jonowens84@gmail.com>
2020-10-02 16:01:30 +0100

    final version

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-async function getJoke(){
-    let response = await fetch("https://icanhazdadjoke.com/", {
+async function getJoke() {
+  let response = await fetch("https://icanhazdadjoke.com/", {
     headers: { accept: "application/json" },
-      });
-    let data = await response.json();
-    console.log(data);
-    h2.innerText = data.joke;
-    upPic.src = "thumbs-up.png";
-    downPic.src = "thumbs-down.png";
+  });
+  let data = await response.json();
+  console.log(data);
+  h2.innerText = data.joke;
+  upPic.src = "thumbs-up.png";
+  downPic.src = "thumbs-down.png";
 }
 let h2 = document.querySelector("h2")
 getJoke()
@@ -34,20 +34,20 @@ moreJokes.addEventListener("click", getJoke)
 
 
 
-function upReaction(){
+function upReaction() {
   // let randomAudioNum = Math.floor(Math.random()*6)+1;
   upPic.src = "harold_up1.jpg"; //random pic generated laugPic${randNum}
   laughingSound.play();
   downPic.src = "";
-  moreJokes.innerText = "Here Come More BANTZ.....";
+  moreJokes.innerText = "click me more more mega BANTZ.....";
 }
 
-function downReaction(){
+function downReaction() {
   // let randomAudioNum = Math.floor(Math.random()*6)
   downPic.src = "harold_down.jpg";
   groanSound.play();
   upPic.src = "";
-  moreJokes.innerText = "Here Come More BANTZ.....";
+  moreJokes.innerText = "Oh come on! click me I got more BANTZ.....";
 }
 // groansound.scr=`groan6.mp3`;
 
