b8c23bb1c416baa2c3adfa17df54df4d092559b6
Jon Owens <jonowens84@gmail.com>
2020-10-02 11:47:34 +0100

    pics added to js

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ let h2 = document.querySelector("h2")
 getJoke()
 // h2.innerText = getJoke();
 
+let upVote = document.querySelector("#upVote");
+let downVote = document.querySelector("#downVote");
+upVote.addEventListener("click");
+downVote.addEventListener("click");
+let upPic = document.querySelector("#upPic");
+let downPic = document.querySelector("#downPic");
+upPic.addEventListener("click")
+downPic.addEventListener("click")
+
+/*write a function to add a reaction to the joke when the viewer clicks upvote
+write a function to add a reaction to the joke when the viewer clicks downvote
+when clicks display corresponding image and sound effect..
+
+if working and is too repetetive add more reactions to add variety.. 
+
+*/
+function upReaction (){
+  upPic
+
+}
