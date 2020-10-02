c21b08b4e965fa37f58079af3323c858b7d5ab83
Jon Owens <jonowens84@gmail.com>
2020-10-02 10:57:34 +0100

    jokes on the page

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("hi");
+
+async function getJoke(){
+    let response = await fetch("https://icanhazdadjoke.com/", {
+        headers: { accept: "application/json" },
+      });
+      let data = await response.json();
+      console.log(data);
+    h2.innerText = data.joke;
+}
+let h2 = document.querySelector("h2")
+getJoke()
+// h2.innerText = getJoke();
+
